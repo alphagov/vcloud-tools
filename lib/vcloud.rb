8a16ac5f6f11db51b4637c626a2aea09b172fca8
Mike Pountney <Mike.Pountney@gmail.com>
2013-12-09 12:49:59 +0000

    pull metadata parsing/extraction code into a general helper

--- lib/vcloud.rb
+++ lib/vcloud.rb
@@ -31,4 +31,31 @@ module Vcloud
     Marshal.load(Marshal.dump(object))
   end
 
+  def self.extract_metadata(metadata)
+    ret = {}
+
+    metadata = [ metadata ] unless metadata.is_a?(Array)
+
+    metadata.each do |entry|
+      # TODO handle pre-5.1 basic Values (TypedValue introduced at 5.1)
+      # TODO potentially just consider everything as a string. Lossy, but then 
+      #      serialising to YAML later is also lossy :(
+      next unless entry.key?(:TypedValue) && entry[:TypedValue].key?(:xsi_type)
+      key = entry[:Key].to_sym
+      val = entry[:TypedValue][:Value]
+      case entry[:TypedValue][:xsi_type]
+        when 'MetadataNumberValue'
+          val = val.to_i
+        when 'MetadataStringValue'
+          val = val.to_s
+        when 'MetadataDateTimeValue'
+          val = DateTime.parse(val)
+        when 'MetadataBooleanValue'
+          val = val == 'true' ? true : false
+      end
+      ret[key] = val
+    end
+    ret
+  end
+
 end
