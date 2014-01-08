bda40aaaee4944cc813880e1b73a2b0f99388b7d
Mike Pountney <Mike.Pountney@gmail.com>
2014-01-08 18:21:00 +0000

    [#59505526] refactor into separate logical config checkers

--- lib/vcloud/config_loader.rb
+++ lib/vcloud/config_loader.rb
@@ -18,30 +18,38 @@ module Vcloud
       raise "#{pre}: config cannot be empty" if config.empty?
 
       if config.key?(:vapps)
-
         vapps = config[:vapps]
-
         vapps.each do |vapp_config|
-
-          raise "#{pre}: vapp config cannot be nil" if vapp_config.nil?
-          raise "#{pre}: vapp config must be a parameter hash" unless vapp_config.is_a? Hash
-          raise "#{pre}: vapp config cannot be empty" if vapp_config.empty?
-
-          [ 'name', 'vdc_name', 'catalog', 'catalog_item'].each do |p|
-            unless vapp_config.key?(p.to_sym) && ! vapp_config[p.to_sym].empty?
-              raise "#{pre}: #{p} must be specified" unless vapp_config.key?(p.to_sym)
-            end
-          end
-
-          if vapp_config.key?(:vm)
-            vm_config = vapp_config[:vm]
-            raise "#{pre}: vm config must be a hash" unless vm_config.is_a? Hash
-            raise "#{pre}: vm config must not be empty" if vm_config.empty?
-          end
+          validate_vapp_config(vapp_config)
         end
-
       end
 
+      config
+    end
+
+    def validate_vapp_config(config)
+      pre = 'ConfigLoader.validate_config'
+      raise "#{pre}: vapp config cannot be nil" if config.nil?
+      raise "#{pre}: vapp config must be a parameter hash" unless config.is_a? Hash
+      raise "#{pre}: vapp config cannot be empty" if config.empty?
+
+      [ 'name', 'vdc_name', 'catalog', 'catalog_item'].each do |p|
+        unless config.key?(p.to_sym) && ! config[p.to_sym].empty?
+          raise "#{pre}: #{p} must be specified" unless config.key?(p.to_sym)
+        end
+      end
+
+      if config.key?(:vm)
+        vm_config = config[:vm]
+        validate_vm_config(vm_config)
+      end
+      config
+    end
+
+    def validate_vm_config(config)
+      pre = 'ConfigLoader.validate_config'
+      raise "#{pre}: vm config must be a hash" unless config.is_a? Hash
+      raise "#{pre}: vm config must not be empty" if config.empty?
       config
     end
 
