117767b38dabba914c9dfecb2a8c3f49a4424c02
Mike Pountney <Mike.Pountney@gmail.com>
2014-01-27 09:00:02 +0000

    EdgeGateway generator working for VirtualServer
    not entirely complete, but enough until we can validate test output against API

--- lib/vcloud/edge_gateway/configuration_generator/load_balancer_service.rb
+++ lib/vcloud/edge_gateway/configuration_generator/load_balancer_service.rb
@@ -30,7 +30,56 @@ module Vcloud
         private
 
         def generate_virtual_server_entry(attrs)
-          {}
+          out = {}
+          out[:IsEnabled] = attrs.key(:enabled) ? attrs[:enabled] : 'true'
+          out[:Name] = attrs[:name]
+          out[:Description] = attrs[:description] || ''
+          out[:Interface] = generate_vs_interface_section(attrs[:network])
+          out[:IpAddress] = attrs[:ip_address]
+          out[:ServiceProfile] = generate_vs_service_profile_section(attrs[:service_profiles])
+          out[:Logging] = attrs.key(:logging) ? attrs[:logging] : 'false'
+          out[:Pool] = attrs[:pool]
+          out
+        end
+
+        def generate_vs_interface_section(network_name)
+          out = {}
+          out[:name] = network_name
+          out[:href] = look_up_network_href(network_name)
+          out
+        end
+
+        def look_up_network_href(name)
+          'http://example.com/api/admin/network/12345678-1234-1234-1234-123456789012'
+        end
+
+        def generate_vs_service_profile_section(attrs)
+          out = []
+          protocols = [ :http, :https, :tcp ]
+          protocols.each do |protocol|
+            out << generate_vs_service_profile_protocol_section(protocol, attrs[protocol])
+          end
+          out
+        end
+
+        def generate_vs_service_profile_protocol_section(protocol, attrs)
+          attrs = {} if attrs.nil?
+          out = {}
+          out[:IsEnabled] = attrs.key?(:enabled) ? attrs[:enabled].to_s : 'false'
+          out[:Protocol] = protocol.to_s.upcase
+          out[:Port] = attrs.key?(:port) ? attrs[:port].to_s : default_port(protocol)
+          out[:Persistence] = generate_vs_persistence_section(protocol, attrs[:persistence])
+          out
+        end
+
+        def default_port(protocol)
+          dp = { http: '80', https: '443', tcp: '' }
+          dp[protocol]
+        end
+
+        def generate_vs_persistence_section(protocol, attrs)
+          attrs = {} if attrs.nil?
+          { Method: '' }
         end
 
         def generate_pool_entry(attrs)
@@ -64,7 +113,6 @@ module Vcloud
 
         def generate_pool_service_port(mode, attrs)
 
-          default_port = { http: '80', https: '443', tcp: '' }
           out = {
             IsEnabled: 'false',
             Protocol: mode.to_s.upcase,
@@ -78,7 +126,7 @@ module Vcloud
             # present, update defaults
             out[:IsEnabled] = attrs[:enabled].to_s if attrs.key?(:enabled)
             out[:Algorithm] = attrs[:algorithm] if attrs.key?(:algorithm)
-            out[:Port]      = attrs.key?(:port) ? attrs[:port].to_s : default_port[mode]
+            out[:Port]      = attrs.key?(:port) ? attrs[:port].to_s : default_port(mode)
             if attrs.key?(:health_check)
               out[:HealthCheckPort] = attrs[:health_check].key?(:port) ?
                   attrs[:health_check][:port] : default_port[mode]
